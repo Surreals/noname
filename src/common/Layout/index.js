3efac16d06379061a4d6332599a73531278ea985
Marian Fedkovych <m.fed@intobi.com>
2023-09-19 20:24:19 +0300

    layout

--- src/common/Layout/index.js
+++ src/common/Layout/index.js
@@ -5,7 +5,7 @@ const { Header, Footer, Content } = AntdLayout;
 
 const contentStyle = {
   textAlign: "center",
-  padding: "0 48px",
+  padding: "8px 32px 0px",
   zIndex: 1,
 };
 const footerStyle = {
@@ -19,20 +19,20 @@ const Layout = ({ children }) => {
 
   return (
     <AntdLayout className="layout">
-      <div>
+      {/* <div>
         <div class="layer layer-1"></div>
         <div class="layer layer-2"></div>
         <div class="layer layer-3"></div>
         <div class="layer layer-4"></div>
         <div class="layer layer-5"></div>
         <div class="layer layer-6"></div>
-      </div>
+      </div> */}
       <Header className="header" style={{ background: "transparent" || colorBgBase }}>
         NONAMEDVIZH
       </Header>
       <Content style={contentStyle}>
         <div
-        className="childrenWrap"
+          className="childrenWrap"
           style={{
             background: colorBgContainer,
             color: colorTextBase,
