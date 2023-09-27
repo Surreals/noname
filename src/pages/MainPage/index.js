943cbd2652448804b054e693e90c0714a1272d8e
Marian Fedkovych <m.fed@intobi.com>
2023-09-27 12:37:10 +0300

    fix

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,14 +1,27 @@
 import { Space, Button, Image } from "antd";
 import Layout from "../../common/Layout";
 import { useNavigate, Link } from "react-router-dom";
-import { StarFilled, FlagFilled, LikeFilled, InstagramOutlined, ThunderboltFilled, FileImageFilled } from "@ant-design/icons";
-import gif from '../../assets/gifs/polar-bear-polar.gif'
+import {
+  StarFilled,
+  FlagFilled,
+  LikeFilled,
+  InstagramOutlined,
+  ThunderboltFilled,
+  FileImageFilled,
+} from "@ant-design/icons";
+import gif from "../../assets/gifs/polar-bear-polar.gif";
 
 import "./index.scss";
 
-const iconStyle = { fontSize: 24, verticalAlign: "baseline"}
+const iconStyle = { fontSize: 24, verticalAlign: "baseline" };
 
 const links = [
+  {
+    title: "TICKETS",
+    route: "/tickets",
+    icon: <StarFilled style={iconStyle} />,
+    props: { type: "primary" },
+  },
   // {
   //   title: "LINE-UP",
   //   route: "/line-up",
@@ -17,28 +30,23 @@ const links = [
   {
     title: "LOCATION",
     link: "https://maps.app.goo.gl/zpDBFm2WZC8rF2od6",
-    icon: <FlagFilled style={iconStyle} />
+    icon: <FlagFilled style={iconStyle} />,
   },
-  {
-    title: "GALLERY",
-    route: "/gallery",
-    icon: <FileImageFilled style={iconStyle} />
-  },
-  {
-    title: "TICKETS",
-    route: "/tickets",
-    icon: <StarFilled style={iconStyle} />,
-    props: { type: "primary" },
-  },
+  // {
+  //   title: "GALLERY",
+  //   route: "/gallery",
+  //   icon: <FileImageFilled style={iconStyle} />
+  // },
+
   {
     title: "DONATE",
     link: "https://send.monobank.ua/3tvYeJLzA6",
-    icon: <LikeFilled style={iconStyle} />
+    icon: <LikeFilled style={iconStyle} />,
   },
   {
     title: "INSTAGRAM",
     link: "https://www.instagram.com/nonamedvizh/?igshid=YmMyMTA2M2Y%3D",
-    icon: <InstagramOutlined style={iconStyle} />
+    icon: <InstagramOutlined style={iconStyle} />,
   },
 ];
 
@@ -50,20 +58,20 @@ const MainPage = () => {
       <Space direction="vertical" size="large" style={{ width: "100%" }}>
         {links.map(({ title, route, props, icon, link }) => (
           <Link to={route || link} target={link && "_blank"}>
-          <Button
-            block
-            // type={"text"}
-            style={{ fontSize: 20, height: "auto" }}
-            icon={icon}
-            // onClick={() => navigate(route)}
-            {...(props || {})}
-          >
-            {title}
-          </Button>
+            <Button
+              block
+              // type={"text"}
+              style={{ fontSize: 20, height: "auto" }}
+              icon={icon}
+              // onClick={() => navigate(route)}
+              {...(props || {})}
+            >
+              {title}
+            </Button>
           </Link>
         ))}
       </Space>
-      <Image width={'70%'} style={{marginTop: 64}} src={gif}/>
+      <Image width={"70%"} style={{ marginTop: 64 }} src={gif} />
     </Layout>
   );
 };
