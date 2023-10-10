b1979d7e3860900e114cb0d9a8f343e55724b326
Marian Fedkovych <m.fed@intobi.com>
2023-10-10 14:52:25 +0300

    lineup

--- src/pages/LineUpPage/index.js
+++ src/pages/LineUpPage/index.js
@@ -1,20 +1,37 @@
-import { Space, Image, Button, Carousel } from "antd";
+import { Image, Button, Carousel } from "antd";
 import Layout from "../../common/Layout";
-import { useEffect, useState } from "react";
-import { InstagramOutlined, SoundOutlined, YoutubeOutlined } from "@ant-design/icons";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import surreal from "../../assets/images/srl.jpg";
-import eshka from "../../assets/images/eshka.jpg";
-import hacked from "../../assets/images/hckedface.jpg";
-import camp from "../../assets/images/camp.jpg";
+import SURR from "../../assets/posters/SURR.jpg";
+import SN from "../../assets/posters/SN.jpg";
+import SICKSOLUTION from "../../assets/posters/SICKSOLUTION.jpg";
+import spotify from "../../assets/social_logos/spotify.png";
+import inst from "../../assets/social_logos/inst.png";
+import youtube from "../../assets/social_logos/youtube.png";
 import "./index.scss";
 
 const artists = [
-  { artist: "Surreal", imgPath: surreal, spoty: "ss", inst: "ss", youtube: "dd" },
-  { artist: "Siberian_Sun", imgPath: camp },
-  { artist: "hackedface", imgPath: hacked },
-  { artist: "Sick_Solution", imgPath: camp },
-  { artist: "ESHKA", imgPath: eshka },
+  {
+    artist: "Surreal",
+    imgPath: SURR,
+    spoty: "https://open.spotify.com/artist/0cpL1ooyiyxpJ4KRz8ILWW?si=cB1ePZXORSiEsYv5Zc5Lsg",
+    inst: "https://www.instagram.com/surreal4you/",
+    youtube: "https://www.youtube.com/@surreal4you",
+  },
+  {
+    artist: "Siberian_Sun",
+    imgPath: SN,
+    spoty: "https://open.spotify.com/artist/4Md7h2zQk811uguAgMZRhz?si=r3WrxzQFSpWB4-O3c3rd-w",
+    inst: "https://www.instagram.com/siberian.sun.official/",
+    youtube: "https://www.youtube.com/@siberiansun7",
+  },
+  {
+    artist: "Sick_Solution",
+    imgPath: SICKSOLUTION,
+    spoty: "https://open.spotify.com/artist/5I8Ea6B50cE6NR6HL6BHa6?si=-_XjaClTTvWvtE92R2TwAQ",
+    inst: "https://www.instagram.com/sicksolutionhc/",
+    youtube: "https://www.youtube.com/@sicksolution3327",
+  },
 ];
 
 const LineUpPage = () => {
@@ -24,11 +41,11 @@ const LineUpPage = () => {
   };
   return (
     <Layout title={"LINE-UP"}>
-      <Carousel autoplay afterChange={onChange}>
+      <Carousel autoplay afterChange={onChange} effect={"fade"}>
         {artists.map(({ artist, imgPath }) => (
           <div>
             <h2>{artist}</h2>
-            <Image width={"100%"} src={imgPath} />
+            <Image width={"100%"} src={imgPath} preview={false} style={{borderRadius: 2}}/>
           </div>
         ))}
       </Carousel>
@@ -39,8 +56,11 @@ const LineUpPage = () => {
             disabled={!artists[slide] || !artists[slide].spoty}
             block
             type="text"
-            icon={<SoundOutlined style={{ fontSize: 54 }} />}
-          />
+            className="socialBtn"
+            // icon={<SoundOutlined style={{ fontSize: 48 }} />}
+          >
+            <img className="socialLogo" src={spotify} alt='Spotify'/>
+          </Button>
         </Link>
 
         <Link to={artists[slide] && artists[slide].youtube} target="_blank">
@@ -48,8 +68,11 @@ const LineUpPage = () => {
             disabled={!artists[slide] || !artists[slide].youtube}
             block
             type="text"
-            icon={<YoutubeOutlined style={{ fontSize: 54 }} />}
-          />
+            className="socialBtn"
+            // icon={<YoutubeOutlined style={{ fontSize: 48 }} />}
+          >
+            <img className="socialLogo" src={youtube} alt='YouTube' />
+          </Button>
         </Link>
 
         <Link to={artists[slide] && artists[slide].inst} target="_blank">
@@ -57,8 +80,11 @@ const LineUpPage = () => {
             disabled={!artists[slide] || !artists[slide].inst}
             block
             type="text"
-            icon={<InstagramOutlined style={{ fontSize: 54 }} />}
-          />
+            className="socialBtn"
+            // icon={<InstagramOutlined style={{ fontSize: 48 }} />}
+          >
+            <img className="socialLogo" src={inst} alt='Instagram'  />
+          </Button>
         </Link>
       </div>
     </Layout>
